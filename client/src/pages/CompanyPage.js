d1b5f0438bf6a1f23b1ee5c44ced1efeb6b6b5ae
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-01-16 18:31:46 +0200

    add data loader

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -1,27 +1,11 @@
 import { useParams } from "react-router";
-import { getCompany } from "../lib/graphql/queries";
-import { useEffect, useState } from "react";
 import JobList from "../components/JobList";
+import { useCompany } from "../lib/graphql/hooks";
+
 
 function CompanyPage() {
   const { companyId } = useParams();
-  const [state, setState] = useState({
-    company: null,
-    loading: true,
-    error: false,
-  });
-  useEffect(() => {
-    (async () => {
-      try {
-        const company = await getCompany(companyId);
-        setState({ company, loading: false, error: false });
-      } catch (error) {
-        console.log("error:", JSON.stringify(error, null, 2));
-        setState({ company: null, loading: false, error: true });
-      }
-    })();
-  }, [companyId]);
-  const { company, loading, error } = state;
+  const { company, loading, error } = useCompany(companyId)
   if (loading) {
     return <div>Loading...</div>;
   }
--- server/db/companies.js
+++ server/db/companies.js
@@ -1,6 +1,15 @@
+import DataLoader from 'dataloader';
 import { connection } from './connection.js';
 
 const getCompanyTable = () => connection.table('company');
+
+export function createCompanyLoader() {
+  return new DataLoader(async (ids) => {
+    console.log('[companyLoader] ids:', ids);
+    const companies = await getCompanyTable().select().whereIn('id', ids);
+    return ids.map((id) => companies.find((company) => company.id === id));
+  });
+}
 
 export async function getCompany(id) {
   return await getCompanyTable().first().where({ id });
