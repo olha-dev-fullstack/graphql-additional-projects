430666a1f5574f5ffa91fa5bbe961f5ad904f85d
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-01-17 09:52:39 +0200

    convert project into typescript

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
-import JobList from "../components/JobList";
-import { useJobs } from "../lib/graphql/hooks";
+import { useState } from 'react';
+import JobList from '../components/JobList';
 import PaginationBar from '../components/PaginationBar';
+import { useJobs } from '../lib/graphql/hooks';
 
 const JOBS_PER_PAGE = 7;
+
 function HomePage() {
   const [currentPage, setCurrentPage] = useState(1);
-  const { jobs, loading, error } = useJobs(
-    JOBS_PER_PAGE,
-    (currentPage - 1) * JOBS_PER_PAGE
-  );
+  const { jobs, loading, error }
+    = useJobs(JOBS_PER_PAGE, (currentPage - 1) * JOBS_PER_PAGE);
+
+  console.log('[HomePage]', { jobs, loading, error });
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -17,13 +18,12 @@ function HomePage() {
     return <div className="has-text-danger">Data unavailable</div>;
   }
   const totalPages = Math.ceil(jobs.totalCount / JOBS_PER_PAGE);
-
   return (
     <div>
-      <h1 className="title">Job Board</h1>
-      <PaginationBar
-        currentPage={currentPage}
-        totalPages={totalPages}
+      <h1 className="title">
+        Job Board
+      </h1>
+      <PaginationBar currentPage={currentPage} totalPages={totalPages}
         onPageChange={setCurrentPage}
       />
       <JobList jobs={jobs.items} />
